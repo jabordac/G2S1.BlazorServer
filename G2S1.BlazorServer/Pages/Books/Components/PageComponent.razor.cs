292b8c492e757a9c990620961f7113d1662a354e
Jorge Alexander Borda Castro <jorge.borda@carvajalservicios.onmicrosoft.com>
2021-09-14 17:30:10 -0500

    Session 02

---
+++ G2S1.BlazorServer/Pages/Books/Components/PageComponent.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+using G2S1.BlazorServer.Data;
+using MudBlazor;
+
+namespace G2S1.BlazorServer.Pages.Books.Components
+{
+    public partial class PageComponent
+    {
+        [Inject] private ISnackbar snackBar { get; set; }
+
+        [CascadingParameter(Name = "Book")] public Book Book { get; set; }
+        [Parameter] public int PageId { get; set; }
+        [Parameter] public EventCallback<int> OnPagePrintClick { get; set; }
+        [Parameter] public RenderFragment TitleFragment { get; set; }
+        [Parameter] public RenderFragment ContentFragment { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //StateHasChanged();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Console.WriteLine($"OnAfterRender: {firstRender}");
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Console.WriteLine("OnParametersSetAsync");
+        }
+
+        protected override bool ShouldRender()
+        {
+            Console.WriteLine("ShouldRender");
+            return true;
+        }
+
+        private void OnPrintClick(int pageId)
+        {
+            OnPagePrintClick.InvokeAsync(pageId);
+        }
+    }
+}
